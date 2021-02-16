451b9e1837448c1f0288bfb5385d5d9679ce7ff6
Atanas Arshinkov <a.arshinkov97@gmail.com>
2021-02-17 00:42:52 +0200

    Prepared create hotel

--- app/src/main/java/com/aarshinkov/mobile/hotelly/activities/HotelCreateActivity.java
+++ app/src/main/java/com/aarshinkov/mobile/hotelly/activities/HotelCreateActivity.java
@@ -5,12 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.aarshinkov.mobile.hotelly.R;
 
 public class HotelCreateActivity extends AppCompatActivity {
+
+    private ImageView hotelCreateImageIV;
+    private EditText hotelCreateNameET;
+    private Spinner hotelCreateCountrySpinner;
+    private EditText hotelCreateCityET;
+    private EditText hotelCreateStreetET;
+    private EditText hotelCreateNumberET;
+    private EditText hotelCreateStarsET;
+    private Button hotelCreateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,23 @@ public class HotelCreateActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Create a hotel");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        hotelCreateImageIV = findViewById(R.id.hotelCreateImageIV);
+        hotelCreateNameET = findViewById(R.id.hotelCreateNameET);
+        hotelCreateCountrySpinner = findViewById(R.id.hotelCreateCountrySpinner);
+        hotelCreateCityET = findViewById(R.id.hotelCreateCityET);
+        hotelCreateStreetET = findViewById(R.id.hotelCreateStreetET);
+        hotelCreateNumberET = findViewById(R.id.hotelCreateNumberET);
+        hotelCreateStarsET = findViewById(R.id.hotelCreateStarsET);
+        hotelCreateBtn = findViewById(R.id.hotelCreateBtn);
+
+        hotelCreateImageIV.setOnLongClickListener(v -> {
+            Toast.makeText(getApplicationContext(), "Image", Toast.LENGTH_LONG).show();
+            return false;
+        });
+
+        hotelCreateBtn.setOnClickListener(v -> {
+
+        });
     }
 
     @Override
